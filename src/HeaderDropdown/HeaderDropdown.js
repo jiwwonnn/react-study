51d28f8eb479e467a37e333e29449537f7f9d97c
jiwwonnn <grayslacks@boomco.org>
2024-07-29 11:20:47 +0900

    header dropdown 2

--- src/HeaderDropdown/HeaderDropdown.js
+++ src/HeaderDropdown/HeaderDropdown.js
@@ -1,14 +1,16 @@
-import '../assets/styles/HeaderDropdown.css'
-import {useState} from "react";
+import '../assets/styles/HeaderDropdown.css';
+import { useState } from "react";
 
 const HeaderDropdown = () => {
+  const [hoveredIndex, setHoveredIndex] = useState(null);
 
+  const handleMouseEnter = (index) => {
+    setHoveredIndex(index);
+  };
 
-  const [isHover, setIsHover] = useState(false)
-
-  const handleMouseOver = () => {
-    setIsHover(true)
-  }
+  const handleMouseLeave = () => {
+    setHoveredIndex(null);
+  };
 
   return (
     <div className='header_wrap'>
@@ -17,89 +19,107 @@ const HeaderDropdown = () => {
           <h1 className="logo">로고</h1>
           <nav className="nav">
             <ul className="gnb_dep1_ul">
-              <li className="gnb_dep1_li">
-                <a href="#">메뉴1</a>
-                <ul className="gnb_dep2_ul">
+              <li
+                className="gnb_dep1_li"
+                onMouseEnter={() => handleMouseEnter(0)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <a href="#">공부</a>
+                <ul className={`gnb_dep2_ul ${hoveredIndex === 0 ? 'show' : ''}`}>
                   <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브1</a>
+                    <a href="#">트럭타기</a>
                   </li>
                   <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브2</a>
+                    <a href="#">공수부대</a>
                   </li>
                   <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브3</a>
-                  </li>
-                </ul>
-              </li>
-
-              <li className="gnb_dep1_li">
-                <a href="#">메뉴2</a>
-                <ul className="gnb_dep2_ul">
-                  <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브1</a>
-                  </li>
-                  <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브2</a>
-                  </li>
-                  <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브3</a>
+                    <a href="#">립밤 바르기</a>
                   </li>
                 </ul>
               </li>
 
-              <li className="gnb_dep1_li">
-                <a href="#">메뉴3</a>
-                <ul className="gnb_dep2_ul">
+              <li
+                className="gnb_dep1_li"
+                onMouseEnter={() => handleMouseEnter(1)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <a href="#">볼캡</a>
+                <ul className={`gnb_dep2_ul ${hoveredIndex === 1 ? 'show' : ''}`}>
                   <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브1</a>
+                    <a href="#">뉴발란스</a>
                   </li>
                   <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브2</a>
+                    <a href="#">손톱</a>
                   </li>
                   <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브3</a>
-                  </li>
-                </ul>
-              </li>
-
-              <li className="gnb_dep1_li">
-                <a href="#">메뉴4</a>
-                <ul className="gnb_dep2_ul">
-                  <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브1</a>
-                  </li>
-                  <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브2</a>
-                  </li>
-                  <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브3</a>
+                    <a href="#">친선전</a>
                   </li>
                 </ul>
               </li>
 
-              <li className="gnb_dep1_li">
-                <a href="#">메뉴5</a>
-                <ul className="gnb_dep2_ul">
+              <li
+                className="gnb_dep1_li"
+                onMouseEnter={() => handleMouseEnter(2)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <a href="#">달력</a>
+                <ul className={`gnb_dep2_ul ${hoveredIndex === 2 ? 'show' : ''}`}>
                   <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브1</a>
+                    <a href="#">탕비실</a>
                   </li>
                   <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브2</a>
+                    <a href="#">가르마펌</a>
                   </li>
                   <li className="gnb_dep2_li">
-                    <a href="#">메뉴1 - 서브3</a>
+                    <a href="#">번지</a>
                   </li>
                 </ul>
               </li>
 
+              <li
+                className="gnb_dep1_li"
+                onMouseEnter={() => handleMouseEnter(3)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <a href="#">에어컨</a>
+                <ul className={`gnb_dep2_ul ${hoveredIndex === 3 ? 'show' : ''}`}>
+                  <li className="gnb_dep2_li">
+                    <a href="#">노트북</a>
+                  </li>
+                  <li className="gnb_dep2_li">
+                    <a href="#">기름종이</a>
+                  </li>
+                  <li className="gnb_dep2_li">
+                    <a href="#">행보관</a>
+                  </li>
+                </ul>
+              </li>
+
+              <li
+                className="gnb_dep1_li"
+                onMouseEnter={() => handleMouseEnter(4)}
+                onMouseLeave={handleMouseLeave}
+              >
+                <a href="#">심사</a>
+                <ul className={`gnb_dep2_ul ${hoveredIndex === 4 ? 'show' : ''}`}>
+                  <li className="gnb_dep2_li">
+                    <a href="#">비</a>
+                  </li>
+                  <li className="gnb_dep2_li">
+                    <a href="#">우산</a>
+                  </li>
+                  <li className="gnb_dep2_li">
+                    <a href="#">크롬</a>
+                  </li>
+                </ul>
+              </li>
             </ul>
           </nav>
           <div className="util">util</div>
         </div>
       </header>
-
     </div>
-  )
-}
+  );
+};
 
-export default HeaderDropdown
+export default HeaderDropdown;
